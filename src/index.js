4d9a2110e8c3fd2206772f9ff7d50b6c8f9914e6
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-04-03 03:21:58 +0600

    timestamp for message format using moment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const http = require("http");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
 
+const { generateMessage } = require("./utils/messages");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -17,8 +19,8 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", (socket) => {
   console.log("New socket connected");
 
-  socket.emit("message", "Welcome!");
-  socket.broadcast.emit("message", "A new user has joined!");
+  socket.emit("message", generateMessage("Welcome!"));
+  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
@@ -27,7 +29,7 @@ io.on("connection", (socket) => {
       return callback("Profanity is not allowed!");
     }
 
-    io.emit("message", message);
+    io.emit("message", generateMessage(message));
     callback();
   });
 
@@ -41,7 +43,7 @@ io.on("connection", (socket) => {
 
   // builtin event
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left!");
+    io.emit("message", generateMessage("A user has left!"));
   });
 });
 
